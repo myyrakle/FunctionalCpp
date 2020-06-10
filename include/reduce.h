0d0fdfcad68e1040018dd19ec437401200cf92a4
myyrakle <sssang97@naver.com>
2020-06-10 22:43:52 +0900

    Update reduce.h

--- include/reduce.h
+++ include/reduce.h
@@ -1,16 +1,20 @@
 #ifndef __FUNCTIONAL_REDUCE__
 #define __FUNCTIONAL_REDUCE__
 
-template <class Container>
-typename Container::value_type reduce(const Container& container)
+template <class Container, class Function>
+typename Container::value_type reduce(const Container& container, Function f)
 {
   using Value = typename Container::value_type;
   
-  Value result = Value{};
+  auto it = container.begin();
+  Value result = *it;
   
-  for(const auto& e : container)
+  it++;
+  
+  while(it!=container.end())
   {
-    result += f(result, e);
+    result = f(result, *it);
+    it++;
   }
   
   return result;
